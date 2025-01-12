0036c40d1fee72e96540ecaad5c73e62156bd3d6
chiefJurist <anthonynnanna004@gmail.com>
2025-01-12 04:45:43 +0100

    dsfgbf

---
+++ magiceden_wallets.py
@@ -0,0 +1,18 @@
+from solana.rpc.api import Client
+
+# Connect to Solana mainnet RPC
+client = Client("https://api.mainnet-beta.solana.com")
+
+# Magic Eden Program ID
+magiceden_program_id = "MEisE1HzehtrDpAAT8PnLHjpSSkRYakotTuJRPjTpo8"
+
+# Fetch recent transactions related to Magic Eden
+response = client.get_signatures_for_address(magiceden_program_id, limit=100)
+
+print("Recent wallet addresses interacting with Magic Eden:")
+for tx in response['result']:
+    signature = tx['signature']
+    transaction = client.get_transaction(signature)
+    if transaction['result']:
+        for account in transaction['result']['transaction']['message']['accountKeys']:
+            print(account)
