ede0f5498e0c2058d8125b9eb703fc0be2b1f4e5
chiefJurist <anthonynnanna004@gmail.com>
2025-01-12 06:19:57 +0100

    wdefghjmk

--- magiceden_wallets.py
+++ magiceden_wallets.py
@@ -14,22 +14,10 @@ magiceden_program_id = Pubkey(decoded_key)  # Convert to Pubkey
 # Initial call to get the first batch of signatures with limit 20
 response = client.get_signatures_for_address(magiceden_program_id, limit=20)
 
-# Set to store unique wallet addresses
-unique_wallets = set()
-
-# Loop through the results and filter only successful transactions
+# Print all the signatures in the response
 if hasattr(response, 'result'):
+    print("Signatures of transactions that interacted with Magic Eden:")
     for tx in response.result:
-        if tx.err is None:  # Successful transaction (no error)
-            signature = tx.signature
-            # Fetch the transaction details
-            transaction = client.get_transaction(signature)
-            if transaction['result']:
-                # Extract the account keys from the transaction
-                for account in transaction['result']['transaction']['message']['accountKeys']:
-                    unique_wallets.add(account)  # Add account to the set of unique wallets
-
-# Display all unique wallet addresses
-print("Wallet addresses that have interacted with Magic Eden:")
-for wallet in unique_wallets:
-    print(wallet)
+        print(tx.signature)
+else:
+    print("No results found.")
